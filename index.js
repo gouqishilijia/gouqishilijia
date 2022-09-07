0a79e91aebb1de61e56660e9db79dc5eee03e928
gouqishilijia <email@example.com>
2022-09-07 12:07:44 +0800

    97777

--- index.js
+++ index.js
@@ -1,82 +1,87 @@
+
 var zuop = [
+    {
+        id: "js",
+        path: "./士力架/Javascript/九宫格抽奖.html",
+        img: "./士力架/img/js/九宫格抽奖.gif",
+        describe: "九宫格抽奖",
+    },
 
     {
         id: "css",
-        path: "/士力架/CSS3/视差效果.html",
-        img: "/士力架/img/css3/视差.gif",
+        path: "./士力架/CSS3/视差效果.html",
+        img: "./士力架/img/css3/视差.gif",
         describe: "视差效果",
     },
+
     {
         id: "css",
-        path: "/士力架/CSS3/loading.html",
-        img: "/士力架/img/css3/loading.gif",
-        describe: "loading",
-    },
-    {
-        id: "css",
-        path: "/士力架/CSS3/Circle.html",
-        img: "/士力架/img/css3/loadingCirle.gif",
+        path: "./士力架/CSS3/Circle.html",
+        img: "./士力架/img/css3/loadingCirle.gif",
         describe: "loadingCircle",
     },
+    {
+        id: "css",
+        path: "./士力架/CSS3/loading.html",
+        img: "./士力架/img/css3/loading.gif",
+        describe: "loading",
+    },
+
 
     {
         id: "jqajax",
-        path: "/士力架/Jquery&ajax/jQ无缝轮播图.html",
-        img: "/士力架/Jquery&ajax/imgs/轮播图.png",
+        path: "./士力架/Jquery&ajax/jQ无缝轮播图.html",
+        img: "./士力架/Jquery&ajax/imgs/轮播图.png",
         describe: "无缝轮播图",
     },
     {
         id: "jqajax",
-        path: "/士力架/Jquery&ajax/三级联动.html",
-        img: "/士力架/Jquery&ajax/imgs/三级联动.png",
+        path: "./士力架/Jquery&ajax/三级联动.html",
+        img: "./士力架/Jquery&ajax/imgs/三级联动.png",
         describe: "三级联动",
     },
     {
         id: "js",
-        path: "/士力架/Javascript/b站.html",
-        img: "/士力架/Javascript/img/b站精灵图.gif",
+        path: "./士力架/Javascript/b站.html",
+        img: "./士力架/Javascript/img/b站精灵图.gif",
         describe: "b站精灵图",
     },
     {
         id: "js",
-        path: "/士力架/Javascript/小米商城轮播图.html",
-        img: "/士力架/Javascript/img/小米.png",
+        path: "./士力架/Javascript/小米商城轮播图.html",
+        img: "./士力架/Javascript/img/小米.png",
         describe: "小米商城轮播图",
     },
+
+
     {
         id: "js",
-        path: "/士力架/Javascript/九宫格抽奖.html",
-        img: "/士力架/img/js/九宫格抽奖.gif",
-        describe: "九宫格抽奖",
-    },
-    {
-        id: "js",
-        path: "/士力架/Javascript/手风琴.html",
-        img: "/士力架/Javascript/img/手风琴.png",
+        path: "./士力架/Javascript/手风琴.html",
+        img: "./士力架/Javascript/img/手风琴.png",
         describe: "手风琴",
     },
     {
         id: "bootstrap",
-        path: "/士力架/BootStrap/儿童/儿童.html",
-        img: "/士力架/BootStrap/儿童/imgs/儿童.png",
+        path: "./士力架/BootStrap/儿童/儿童.html",
+        img: "./士力架/BootStrap/儿童/imgs/儿童.png",
         describe: "关于儿童",
     },
     {
         id: "bootstrap",
-        path: "/士力架/BootStrap/整形美容/整形美容.html",
-        img: "/士力架/BootStrap/整形美容/images/整形.png",
+        path: "./士力架/BootStrap/整形美容/整形美容.html",
+        img: "./士力架/BootStrap/整形美容/images/整形.png",
         describe: "整形美容",
     },
     {
         id: "hfive",
-        path: "/士力架/HTML5/复古music播放器.html",
-        img: "/士力架/img/html5/复古/复古.gif",
+        path: "./士力架/HTML5/复古music播放器.html",
+        img: "./士力架/img/html5/复古/复古.gif",
         describe: "复古音乐播放器",
     },
     {
         id: "hfive",
-        path: "/士力架/HTML5/图表.html",
-        img: "/士力架/img/html5/图表/图表.png",
+        path: "./士力架/HTML5/图表.html",
+        img: "./士力架/img/html5/图表/图表.png",
         describe: "图表",
     },
 
@@ -84,7 +89,7 @@ var zuop = [
 
 $(".menu li").click(function () {
     // console.log(typeof $(this).attr("id"));
-    $(".zuop").html(" ")
+    $(".showWork").html(" ")
     // $(this).attr("id")
     var _this = $(this).attr("id")
     if (_this == "all") {
@@ -93,18 +98,18 @@ $(".menu li").click(function () {
     zuop.map(function (obj) {
         // console.log(obj.id);
         if (obj.id === _this) {
-            $(".zuop").append(`
-            <div class="he"><a href="${obj.path}"><img src="${obj.img}" alt=""><p>${obj.describe}</p></a></div>
-            `)
+            $(".zuop .showWork").append(`
+        <div class="he"><a href="${obj.path}"><img src="${obj.img}" alt=""><p>${obj.describe}</p></a></div>
+        `)
         }
     })
 })
 
 function all12() {
     zuop.map(function (obj) {
-        $(".zuop").append(`
-        <div class="he"><a href="${obj.path}"><img src="${obj.img}" alt=""><p>${obj.describe}</p></a></div>
-        `)
+        $(".zuop .showWork").append(`
+    <div class="he"><a href="${obj.path}"><img src="${obj.img}" alt=""><p>${obj.describe}</p></a></div>
+    `)
     })
 }
 all12()
